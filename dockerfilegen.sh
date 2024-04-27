092f5fb02b93ea4bdbe606c3cb8428f52308ed20
simon <130985998+SimonStiil@users.noreply.github.com>
2024-04-27 13:30:02 +0200

    making stuff arm compatible

---
+++ dockerfilegen.sh
@@ -0,0 +1,8 @@
+source package.env
+echo -n "FROM ${PACKAGE_CONTAINER_BASE}
+WORKDIR ${PACKAGE_CONTAINER_WORKDIR}
+COPY ca-certificates.crt /etc/ssl/certs/
+COPY ${PACKAGE_CONTAINER_APPLICATION}-${1} ${PACKAGE_CONTAINER_DESTINATION}
+LABEL org.opencontainers.image.source ${PACKAGE_CONTAINER_SOURCE}
+LABEL org.opencontainers.image.licenses ${PACKAGE_CONTAINER_LICENSE}
+ENTRYPOINT [\"${PACKAGE_CONTAINER_APPLICATION}\"]" >Dockerfile
